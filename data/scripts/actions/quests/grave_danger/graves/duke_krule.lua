ea57adf0e0c814c191abc90409d8ea0eb6127c2d
QuebradaZN <47014952+QuebradaZN@users.noreply.github.com>
2020-11-30 13:44:53 -0300

    Update duke_krule.lua

--- data/scripts/actions/quests/grave_danger/graves/duke_krule.lua
+++ data/scripts/actions/quests/grave_danger/graves/duke_krule.lua
@@ -6,21 +6,21 @@ local setting = {
 	playerTeleport = {x = 33456, y = 31477, z = 13} -- pra onde player vai
 }
 
-local vlarkorthLever = Action()
+local DukeKruleLever = Action()
 
 -- Start Script
-function vlarkorthLever.onUse(creature, item, fromPosition, target, toPosition, isHotkey)
+function DukeKruleLever.onUse(creature, item, fromPosition, target, toPosition, isHotkey)
 	if item.itemid == 9825 and item.actionid == 57610 then
-	local clearVlarkorthRoom = Game.getSpectators(Position(setting.centerRoom), false, false, 11, 11, 11, 11)       
-	for index, spectatorcheckface in ipairs(clearVlarkorthRoom) do
+	local clearDukeKruleRoom = Game.getSpectators(Position(setting.centerRoom), false, false, 11, 11, 11, 11)       
+	for index, spectatorcheckface in ipairs(clearDukeKruleRoom) do
 		if spectatorcheckface:isPlayer() then
 			creature:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Someone is fighting against the boss! You need wait awhile.")
 			return false
 		end
 	end	
-	for index, removeVlarkorth in ipairs(clearVlarkorthRoom) do
-		if (removeVlarkorth:isMonster()) then
-			removeVlarkorth:remove()
+	for index, removeDukeKrule in ipairs(clearDukeKruleRoom) do
+		if (removeDukeKrule:isMonster()) then
+			removeDukeKrule:remove()
 		end
 	end
 		Game.createMonster("Duke Krule", setting.bossPosition, false, true)
@@ -36,9 +36,9 @@ function vlarkorthLever.onUse(creature, item, fromPosition, target, toPosition, 
 			setPlayerStorageValue(player,setting.storage, os.time() + 20 * 60 * 60)
             player:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'You have 20 minute(s) to defeat the boss.')
 				addEvent(function()
-					local spectatorsVlarkorth = Game.getSpectators(Position(setting.centerRoom), false, false, 11, 11, 11, 11)
-						for u = 1, #spectatorsVlarkorth, 1 do
-							if spectatorsVlarkorth[u]:isPlayer() and (spectatorsVlarkorth[u]:getName() == player:getName()) then
+					local spectatorsDukeKrule = Game.getSpectators(Position(setting.centerRoom), false, false, 11, 11, 11, 11)
+						for u = 1, #spectatorsDukeKrule, 1 do
+							if spectatorsDukeKrule[u]:isPlayer() and (spectatorsDukeKrule[u]:getName() == player:getName()) then
 								player:teleportTo(Position(setting.kickPosition))
 								player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 								player:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'Time is over.')
@@ -50,5 +50,5 @@ function vlarkorthLever.onUse(creature, item, fromPosition, target, toPosition, 
 	return true
 end
 
-vlarkorthLever:aid(57610)
-vlarkorthLever:register()
+DukeKruleLever:aid(57610)
+DukeKruleLever:register()
